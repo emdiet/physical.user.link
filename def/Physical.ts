c863ab0295934ae78c4e3d86718de0d9f1e65c60
emdiet <mortificaris@gmail.com>
2019-02-22 18:55:37 +0100

    turned public field into setter

--- def/Physical.ts
+++ def/Physical.ts
@@ -5,7 +5,7 @@ export interface Physical {
     request() : SYNQ;
     respond(synq : SYNQ) : ACK;
     open(ack : ACK) : void;
-    onMessage : (message : string) => void;
+    setOnMessage(f : (message : string) => void) : void;
     send(message : string) : void;
     close() : void;
 }
